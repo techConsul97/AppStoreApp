c84c98524b48d72aac804cbead1e0101fc670a1e
techConsul97 <ittechconsultantemail@gmail.com>
2022-12-06 16:34:05 +0000

    Ceated Main Screen

--- app/src/main/java/com/sebqv97/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/sebqv97/myapplication/presentation/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sebqv97.myapplication.presentation.apps_list.MainScreen
-import com.sebqv97.myapplication.presentation.ui.theme.MyApplicationTheme
+import com.sebqv97.myapplication.presentation.ui.theme.AptoideTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,28 +19,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+           AptoideTheme() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
-                    MainScreen()
+                    Scaffold(topBar = ) {
+                        
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
+
 }
